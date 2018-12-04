ff51c625ca9a5c9104ff1a651d225f71f57a2197
Stayko Chalakov <chalakov88@gmail.com>
2018-12-04 19:53:44 +0200

    Add components for different views

---
+++ src/components/DayView.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface Props {}
+
+export interface State {}
+
+export default class DayView extends React.Component<Props, State> {
+  state = {};
+  render() {
+    return <div>DayView</div>;
+  }
+}
---
+++ src/components/ListView.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface Props {}
+
+export interface State {}
+
+export default class ListView extends React.Component<Props, State> {
+  state = {};
+  render() {
+    return <div>ListView</div>;
+  }
+}
